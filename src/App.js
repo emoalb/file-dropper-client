56637ce97674ca2363bb20f82cdf7a725153f447
emoalb <e_alb@abv.bg>
2021-04-04 20:27:14 +0300

    Getting started with routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {useRoutes} from 'hookrouter';
 import UploadPage from "./UploadPage";
 import AppBar from "./AppBar";
+import NotFoundPage from "./NotFoundPage";
 //const urlIP = "87.120.103.179:8000"
 //const urlLocalhost = "localhost:8000"
+const routes = {
+    '/': () => <UploadPage/>,
+}
 const App = (props) => {
+    const routeResult = useRoutes(routes);
     return (
-        <div className={'app'}>
+        <div className="App">
             <AppBar/>
-            <UploadPage/>
+            {routeResult||<NotFoundPage/>}
         </div>
     )
 }
---
+++ src/NotFoundPage.js
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+    return (
+        <span>
+        <h1>404</h1>
+        <h2>Not Found</h2>
+            </span>
+    )
+
+}
+export  default NotFoundPage;
